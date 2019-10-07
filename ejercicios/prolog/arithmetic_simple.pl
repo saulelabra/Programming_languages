33f372066f576adc73c40f05aca30e639a770876
saulelabra <quiquelabra@gmail.com>
2019-10-07 12:58:43 -0500

    Clase prolog

---
+++ ejercicios/prolog/arithmetic_simple.pl
@@ -0,0 +1,28 @@
+/*
+Simple example of using arithmetic in Prolog
+Saúl Enrique Labra Cruz
+*/
+
+%To make a query a variable has to be given to return the result
+c_to_f(C, F) :-
+    F is C * (9 / 5) + 32.
+
+f_to_c(F, C) :-
+    C is (F - 32) * 5 / 9.
+
+quadratic(A, B, C, X1, X2) :-
+    D is B * B - 4 * A * C,
+    D >= 0,
+    X1 is (-B + sqrt(D)) / (2 * A),
+    X2 is (-B - sqrt(D)) / (2 * A).
+
+%Factorial
+%Base Case
+factorial(0, 1).
+
+%Recursive rule, N = Input, R = Result
+factorial(N, R) :-
+    N > 0,
+    N1 is N - 1,
+    factorial(N1, S),%Inside parenthesis expressions are not valid, only variables
+    R is N * S.
--- ejercicios/prolog/family.pl
+++ ejercicios/prolog/family.pl
@@ -14,12 +14,14 @@ father(bardock, goku).
 father(bardock, raditz).
 father(vegeta, trunks).
 father(vegeta, bulla).
+father(dr_brief, bulma).
 mother(gine, goku).
 mother(gine, raditz).
 mother(chichi, gohan).
 mother(chichi, goten).
 mother(bulma, trunks).
 mother(bulma, bulla).
+mother(panchy, bulma).
 male(bardock).
 male(goku).
 male(vegeta).
@@ -36,7 +38,8 @@ brothers(X, Y) :-
     father(Z, X),
     father(Z, Y),
     mother(W, X),
-    mother(W, Y).
+    mother(W, Y),
+    X \= Y.
 
 brother(X, Y) :-
     brothers(X, Y),
@@ -46,10 +49,21 @@ sister(X, Y) :-
     brothers(X, Y),
     female(Y).
 
+/*It is possible to have the same name with different functions as long as they are contiguous
+They execute sequentialy*/
+
 grandfather(X, Y) :-
     male(X),% X is male
     father(Z,Y),% Z is father of Y
     father(X,Z). % X is father of Z
+
+grandfather(X, Y) :-
+    father(X, Z),
+    mother(Z, Y).
+
+grandfather(X, Y) :-
+    father(X, Z),
+    father(Z, Y).
 
 grandmother(X, Y) :-
     female(X),% X is female
